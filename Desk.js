2dd8499dffa85a1f47b70924f65e04c339783f4a
aanyos <85759900+aanyos@users.noreply.github.com>
2022-11-09 20:32:30 -0600

    Add files via upload

---
+++ Desk.js
@@ -0,0 +1,54 @@
+
+img = "";
+mystatus = "";
+
+objects = [];
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("Deskstatus").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    mystatus = true;
+    objectDetector.detect(img, gotResult)
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+
+
+function preload()
+{
+    img = loadImage("desk.jpg");
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+    
+    if(mystatus != "")
+    {
+        for(var i = 0; i <objects.length; i++) {
+            document.getElementById("Deskstatus").innerHTML = "Status : Object Detected";
+
+            fill(255, 0, 0);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(255, 0, 0);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+    
+}
